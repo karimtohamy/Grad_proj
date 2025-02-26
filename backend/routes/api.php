6371c3f4739d46fae88472b4a6746bdfb79c5391
karimtohamy <karimbassemtohamy@gmail.com>
2025-02-26 14:24:50 +0200

    localization middleware on routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,19 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Customer\HomepageController;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Ramsey\Uuid\Generator\RandomGeneratorFactory;
 
 
-Route::middleware('guest')->group(function () {
+Route::middleware([SetLocale::class])->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
 
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum',SetLocale::class])->group(function () {
     Route::get('/',[HomepageController::class,'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
