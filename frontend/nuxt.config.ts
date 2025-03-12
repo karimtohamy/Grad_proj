8d2358f07a62d55d97c9842921d1cef0e8dc8133
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-12 17:23:41 +0200

    axios and pinia inn

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,32 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE || "http://localhost:3000/api",
+    },
+  },
+  compatibilityDate: "2024-11-01",
 
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxtjs/ionic'
-  ],
-  ssr:false,
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/ionic", "@pinia/nuxt"],
+  ssr: false,
+  css: ["~/assets/css/main.css"],
 
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
   ionic: {
     integrations: {
       //
     },
     css: {
-      basic:false,
-      core:false
+      basic: false,
+      core: false,
     },
-    config: {
-      //
-    }
   },
-
-})
+});
---
+++ frontend/plugins/axios.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useAuthStore } from '~/stores/auth'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const runtimeConfig = useRuntimeConfig();
+  const axiosInstance = axios.create({
+    baseURL: runtimeConfig.public.apiBase,
+  });
+
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      config.headers['accept-language'] = 'en';
+      const token = useAuthStore().token;
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        useAuthStore().logout();
+        sessionStorage.removeItem("token");
+        navigateTo("/auth/login"); // Redirect to your login page
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return {
+    provide: { axios: axiosInstance },
+  };
+});
