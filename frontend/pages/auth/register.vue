40391a550fc239c1d152679baacd6aaa829ef79d
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-12 17:25:51 +0200

    auth pages

---
+++ frontend/pages/auth/register.vue
@@ -0,0 +1,65 @@
+<template>
+    <IonPage>
+        <form @submit.prevent="register" class="space-y-4">
+            <!-- Email -->
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.email" placeholder="Email">
+
+            <!-- Name in English -->
+            <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name in
+                English</label>
+            <input type="text" id="firstName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.name_en" placeholder="Name in English">
+
+            <!-- Name in Arabic -->
+            <label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name in
+                Arabic</label>
+            <input type="text" id="lastName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.name_ar" placeholder="Name in Arabic">
+
+            <!-- Phone Number -->
+            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+            <input type="text" id="phone"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.phone" placeholder="Phone Number">
+
+            <!-- Password -->
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <input type="password" id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.password" placeholder="Password">
+
+            <!-- Confirm Password -->
+            <label for="passwordConfirmation"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+            <input type="password" id="passwordConfirmation"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="user.password_confirmation" placeholder="Confirm Password">
+
+            <!-- Error Message -->
+            <div class="text-red-500 text-sm">{{ errMsg }}</div>
+
+            <!-- Submit Button -->
+            <button type="submit"
+                class="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">Submit</button>
+        </form>
+
+
+    </IonPage>
+</template>
+<script setup>
+definePageMeta({ layout: 'authlayout' })
+const user = ref({})
+const errMsg = ref('')
+const userAuth = useAuthStore()
+
+function register() {
+    return userAuth.register(user.value)
+        .then((res) => {
+        })
+}
+</script>
