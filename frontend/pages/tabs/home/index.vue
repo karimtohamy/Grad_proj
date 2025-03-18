d2b6392c3cba289afb1a88d96cfda1a6c4985be4
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-18 02:21:19 +0200

    home page

---
+++ frontend/pages/tabs/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <IonPage >
+    <NavBar/>
+    <IonContent class="custom-content">
+
+      <Services :service="services"/>
+      <Featured :featured="featured"/>
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup>
+definePageMeta({
+  alias: ['/','/tabs/home'],
+})
+ const serviceStore = useServicesStore()
+ const services = ref([])
+ const featured = ref([])
+ const loading = ref(false)
+  function getHomePageData(){
+    serviceStore.getHomepageData()
+    .then((res)=>{
+      services.value = res.services
+      featured.value = res.featured_providers
+    })
+  }
+ onIonViewDidEnter(async () => {
+  getHomePageData()
+   
+ })
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
