d06b7a849267aa25fcb3a2fc8cc955714e9f0e51
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-18 23:52:22 +0200

    spashscreen added

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,15 +1,35 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'nuxt-app',
-  webDir: 'dist',
+  appId: "io.ionic.Manzely",
+  appName: "Manzely",
+  webDir: "dist",
   server: {
-    androidScheme: 'http'
+    androidScheme: "http",
   },
   plugins: {
-
-  }
+    SplashScreen: {
+      launchShowDuration: 500,
+      launchAutoHide: true,
+      launchFadeOutDuration: 300,
+      backgroundColor: "#eff6e0",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      useDialog: true,
+      alwaysShowSplash: true, // Ensure splash screen is always displayed
+    },
+    StatusBar: {
+      overlaysWebView: false,
+      backgroundColor: "#eff6e0",
+    },
+  },
 };
 
 export default config;
