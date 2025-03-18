76d345903ac0bbe0d2a2f06593f8a30f08e9e330
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-18 02:21:01 +0200

    services component

---
+++ frontend/components/services.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <h4 class="text-main text-start z-10 w-full ps-6">
+      Our Services
+    </h4>
+  <div class="flex justify-center w-full mt-5">
+    <div class="overflow-x-auto snap-x snap-mandatory p-4 scroll-smooth mx-auto lg:w-full">
+      <div class="grid grid-rows-2 grid-flow-col auto-cols-max gap-4 lg:justify-center">
+        <div v-for="service in props.service" :key="service.id"
+          class="bg-white rounded-lg p-4 w-[28vw] lg:w-30 h-20 text-center flex items-center justify-center snap-center">
+          {{ service.name }}
+        </div>
+        <div class="w-0.5 lg:w-0"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+
+<script setup>
+const props = defineProps({
+  service: Object
+})
+</script>
+<style scoped></style>
