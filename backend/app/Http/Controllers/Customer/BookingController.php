ba1e2368c171aed944b7ce41733c8c641b939d39
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-20 05:55:19 +0200

    booking controller init

---
+++ backend/app/Http/Controllers/Customer/BookingController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\BookingStatus;
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'service_provider_id'=>'required|exists:service_providers,id',
+            'time'=>'required',
+
+        ]);
+        $customer_id= auth()->user()->customer->id;
+        Booking::create([
+            'status'=> BookingStatus::Pending,
+            'customer_id'=>$customer_id,
+            'service_provider_id'=>$request->service_provider_id,
+        ]);
+    }
+}
