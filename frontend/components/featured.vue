88a8e79b0096837582bb7522882406b8188da657
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-20 14:30:17 +0200

    skeleton component for service providers

--- frontend/components/featured.vue
+++ frontend/components/featured.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="w-full">
     <h4 class="ps-6 text-main">Featured service providers</h4>
-    
-    <div v-if="loading" class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 p-6">
-      <div v-for="n in 6" :key="n" class="bg-gray-300 animate-pulse flex items-center rounded-lg p-4  shadow-md">
-        <div class="size-14 bg-gray-400 rounded-full aspect-square"></div>
-        <div class="flex flex-col justify-center space-y-2 ps-8 w-full">
-          <div class="h-4 bg-gray-400 rounded w-3/4"></div>
-          <div class="h-4 bg-gray-400 rounded w-1/2"></div>
-          <div class="flex items-center space-x-2 mt-1">
-            <div class="h-4 bg-gray-400 rounded w-5"></div>
-            <div class="h-4 bg-gray-400 rounded w-10"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ProviderSkeleton v-if="loading"/>
 
     <div v-else class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 p-6">
       <div @click="navigate(provider.id)" v-for="provider in props.featured" :key="provider.id" class="bg-white flex items-center rounded-lg p-4 shadow-md space-x-8">
--- frontend/pages/tabs/home/service_[id].vue
+++ frontend/pages/tabs/home/service_[id].vue
@@ -2,10 +2,12 @@
   <IonPage>
 
     <IonContent>
-      <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-6 px-4">
+      <ProviderSkeleton v-if="loading"/>
+      <div v-else class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-6 px-4">
 
-        <div v-for="provider in providers" class="bg-white flex items-center rounded-lg p-4 shadow-md space-x-8 ">
-          <img src="https://avatar.iran.liara.run/public/25" alt="" class="w-12 h-12 rounded-full">
+        <div  @click="navigate(provider.id)" v-for="provider in providers"
+          class="bg-white flex items-center rounded-lg p-4 shadow-md space-x-8 ">
+          <img :src="provider.image" lazy alt="" class="w-12 h-12 rounded-full">
           <div class="flex flex-col justify-center">
             <div class="font-semibold text-main">{{ provider.name }}</div>
             <div class="text-sm text-main">{{ provider.service_name }}</div>
@@ -25,16 +27,23 @@
 
 <script setup>
 const route = useRoute();
+const router = useIonRouter()
 const servicesStore = useServicesStore()
 const serviceId = computed(() => route.params.id || null);
 const providers = ref({});
-const loading = ref(false)
+const loading = ref(true)
 onIonViewWillEnter(() => {
-  servicesStore.fetchServiceProviders(serviceId.value)
-    .then((res) => {
-      providers.value = res
-    })
+
+  if (serviceId != null) {
+    servicesStore.fetchServiceProviders(serviceId.value)
+      .then((res) => {
+        providers.value = res
+        loading.value = false
+      })
+  }
 
 })
-
+function navigate(providerId) {
+  router.push({ name: 'tabs-home-provider_id', params: { id: providerId } })
+}
 </script>
