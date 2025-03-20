5e4631dc4165ae9bcb6df0bd1448bfd715561a7c
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-20 05:47:55 +0200

    enahnced middleware

--- frontend/middleware/auth.global.js
+++ frontend/middleware/auth.global.js
@@ -1,11 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useAuthStore().$state.token
   const router = useIonRouter();
+
+  if (to.name === 'auth-login' && from.name === null) { 
+    // Allow direct navigation to the login page without interference
+    return;
+  }
+
   if (to.name.includes('auth') && token) {
-    return router.push('/')
+    return router.push('/');
   }
 
   if (!to.name.includes('auth') && !token) {
-    return router.push('/auth/login')
+    return router.push('/auth/login');
   }
 });
