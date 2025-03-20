e47948a8e50dcf0d9c532c46c1c787bc493f3f6b
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-20 05:46:31 +0200

    fixed nvigation bug

--- frontend/pages/tabs.vue
+++ frontend/pages/tabs.vue
@@ -1,27 +1,26 @@
 <template>
+    
     <IonTabs>
-        <IonRouterOutlet class="" />
+
+        <IonRouterOutlet />
         <IonTabBar slot="bottom" class="flex justify-around bg-main py-2  text-background shadow-md">
-            <IonTabButton tab="home" href="/tabs/home"
-                class="flex flex-col items-center bg-main ">
+            <IonTabButton @click="goToHome" class="flex flex-col items-center bg-main ">
                 <ion-icon :icon="ioniconsHomeOutline" class="text-background"></ion-icon>
                 <IonLabel class="text-background">Home</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="profile" href="/tabs/profile"
-                class="flex flex-col items-center  bg-main ">
+
+            <IonTabButton tab="profile" href="/tabs/profile" class="flex flex-col items-center  bg-main ">
                 <ion-icon :icon="ioniconsPersonOutline" class="text-background"></ion-icon>
                 <IonLabel class="text-background">Profile</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="chat" href="/tabs/chat"
-                class="flex flex-col items-center  bg-main ">
+            <IonTabButton tab="chat" href="/tabs/chat" class="flex flex-col items-center  bg-main ">
                 <ion-icon :icon="ioniconsChatbubblesOutline" class="text-background"></ion-icon>
                 <IonLabel class="text-background">Chat</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="bookings" href="/tabs/bookings"
-                class="flex flex-col items-center  bg-main ">
+            <IonTabButton tab="bookings" href="/tabs/bookings" class="flex flex-col items-center  bg-main ">
                 <ion-icon :icon="ioniconsCalendarOutline" class="text-background"></ion-icon>
                 <IonLabel class="text-background">Bookings</IonLabel>
             </IonTabButton>
@@ -34,4 +33,10 @@
 </template>
 
 <script setup>
+const router = useIonRouter();
+
+function goToHome() {
+    router.replace('/tabs/home'); // Forces the router to navigate to 'home/index.vue'
+}
+
 </script>
