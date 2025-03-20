13d5e8fc481c65ec97fb6415ce6f6b51750b5b61
karimtohamy <karimbassemtohamy@gmail.com>
2025-03-20 05:48:49 +0200

    providers listing and provider pages init

---
+++ frontend/pages/tabs/home/provider_[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <IonPage>
+    <IonContent>
+      {{ providers }}
+    </IonContent>
+  </IonPage>
+</template>
+
+<script  setup>
+const route = useRoute();
+const servicesStore = useServicesStore()
+const serviceProviderId = computed(() => route.params.id || null);
+const providers = ref([]);
+const loading = ref(false)
+onIonViewWillEnter(() => {
+  servicesStore.fetchServiceProvider(serviceProviderId.value)
+    .then((res) => {
+      providers.value = res
+    })
+
+})
+</script>
+
+<style>
+
+</style>
---
+++ frontend/pages/tabs/home/service_[id].vue
@@ -0,0 +1,40 @@
+<template>
+  <IonPage>
+
+    <IonContent>
+      <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-6 px-4">
+
+        <div v-for="provider in providers" class="bg-white flex items-center rounded-lg p-4 shadow-md space-x-8 ">
+          <img src="https://avatar.iran.liara.run/public/25" alt="" class="w-12 h-12 rounded-full">
+          <div class="flex flex-col justify-center">
+            <div class="font-semibold text-main">{{ provider.name }}</div>
+            <div class="text-sm text-main">{{ provider.service_name }}</div>
+            <div class="flex items-center mt-1 text-yellow-500">
+              <ion-icon :icon="ioniconsStar" class="text-lg"></ion-icon>
+              <span class="ml-1 text-sm text-[#598392]">{{ provider.received_reviews_avg_rating }}</span>
+              <span class="text-sm flex items-center text-[#598392]">({{ provider.received_reviews_count }}
+                Reviews)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup>
+const route = useRoute();
+const servicesStore = useServicesStore()
+const serviceId = computed(() => route.params.id || null);
+const providers = ref({});
+const loading = ref(false)
+onIonViewWillEnter(() => {
+  servicesStore.fetchServiceProviders(serviceId.value)
+    .then((res) => {
+      providers.value = res
+    })
+
+})
+
+</script>
