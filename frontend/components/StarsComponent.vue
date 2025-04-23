c1ffd7d3dd9e92e28e4366ba28e0609666c2dd7b
karimtohamy <karimbassemtohamy@gmail.com>
2025-04-23 19:25:22 +0200

    service provider page

---
+++ frontend/components/StarsComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex items-center" >
+
+    <div class="text-sm text-main me-2">
+      {{ count }}
+      reviews
+    </div>
+    <div class="stars-landing" :style="{ '--rating': rating }" aria-label="Rating of this product">
+      ★★★★★
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+const props = defineProps({
+  rating: {
+    type: Number,
+    required: true,
+  },
+  count: {
+    type: Number ?? null,
+    required: true
+  }
+})
+</script>
+
+<!-- Keep style unscoped -->
+<style>
+.stars-landing {
+  --star-size: 1.5rem;
+  --star-color: #b6b7b9;
+  --star-background: #facc15;
+  --percent: calc(var(--rating) / 5 * 100%);
+
+  font-size: var(--star-size);
+  font-family: monospace, 'Arial', sans-serif;
+  color: var(--star-color);
+  letter-spacing: 0.0001rem;
+  line-height: 1;
+  /* Removes vertical spacing */
+  display: inline-block;
+  /* Keeps container tight */
+  position: relative;
+  vertical-align: middle;
+  /* Helps in centering */
+}
+
+.stars-landing::before {
+  content: "★★★★★";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  letter-spacing: 0.0001rem;
+  background: linear-gradient(90deg,
+      var(--star-background) 0%,
+      var(--star-background) var(--percent),
+      var(--star-color) var(--percent),
+      var(--star-color) 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  pointer-events: none;
+}
+</style>
--- frontend/pages/tabs/home/provider_[id].vue
+++ frontend/pages/tabs/home/provider_[id].vue
@@ -1,26 +1,67 @@
 <template>
   <IonPage>
     <IonContent>
-      {{ providers }}
+      <div class="px-5 pt-5">
+
+        <div class=" flex justify-between items-center pt-5">
+
+          <div class="flex items-center">
+            <div class="rounded-full me-3 aspect-square border-main border-1 flex items-center justify-center h-8" @click="goback">
+              <IonIcon :icon="ioniconsArrowBack" class="text-main" />
+            </div>
+            <img :src="provider.image" alt="" loading="lazy" class="size-16">
+          </div>
+
+          <div class="flex flex-col items-center">
+
+            <span class="underline text-main underline-main text-2xl">{{ provider.name }}</span>
+            <StarsComponent :rating="provider.rating" :count="provider.rating_count" />
+          </div>
+
+        </div>
+        <!-- where comments on teh provider will apear -->
+        <div v-if="provider.comments">
+          some comments
+        </div>
+        <div :class="{ 'mt-64': !provider.comments }">
+          <button class=" h-9 !px-3 flex items-center justify-center mx-auto !rounded-full bg-blue-400"
+            @click="book(provider.id)">
+            Book now >
+          </button>
+        </div>
+        {{ provider }}
+      </div>
+
     </IonContent>
   </IonPage>
 </template>
 
-<script  setup>
-const route = useRoute();
+<script setup>
+import { ref, computed } from 'vue'
+import { IonPage, IonContent, onIonViewWillEnter } from '@ionic/vue'
+import { star as ioniconsStar, starOutline as ioniconsStarOutline } from 'ionicons/icons'
+import { useRoute } from 'vue-router'
+import { useServicesStore } from '@/stores/services'
+import { StarsComponent } from '#components'
+
+const route = useRoute()
 const servicesStore = useServicesStore()
-const serviceProviderId = computed(() => route.params.id || null);
-const providers = ref([]);
+const serviceProviderId = computed(() => route.params.id || null)
+const provider = ref({})
 const loading = ref(false)
+const router = useIonRouter()
+
 onIonViewWillEnter(() => {
   servicesStore.fetchServiceProvider(serviceProviderId.value)
     .then((res) => {
-      providers.value = res
+      provider.value = res
     })
-
 })
+
+const goback = ()=>{
+    router.back()
+}
 </script>
 
-<style>
 
-</style>
+<style></style>
